814d6458c427d5e38b356808bd5715669273551d
zobithecat <ubuntu@ip-172-26-9-104.ap-northeast-2.compute.internal>
2023-09-26 09:10:48 +0000

    changed index for daily and monthly aggregation, extract region value on config.json

--- main.py
+++ main.py
@@ -19,17 +19,20 @@ results = subprocess.run(cmd, shell=True, capture_output=True, encoding="utf-8")
 stdout = results.stdout
 vnstat = EasyDict(loads(stdout))
 traffic = vnstat.interfaces[0].traffic
+dayLastIdx = len(traffic.day) - 1
+monLastIdx = len(traffic.month) - 1
 
 async def send_message(text):
 	await bot.sendMessage(chat_id = config.chat_id, text = text)
 
-message = "(daily) " + str(traffic.day[0].date.year) + "-" + str(traffic.day[0].date.month) + "-" + str(traffic.day[0].date.day) + "\n"
-message += "received: " + str(round(traffic.day[0].rx / (1024 * 1024),1)) + "MB\n"
-message += "transmitted: " + str(round(traffic.day[0].tx / (1024 * 1024), 1)) + "MB\n"
+message = "region: " + config.region
+message += "(daily) " + str(traffic.day[dayLastIdx].date.year) + "-" + str(traffic.day[dayLastIdx].date.month) + "-" + str(traffic.day[dayLastIdx].date.day) + "\n"
+message += "received: " + str(round(traffic.day[dayLastIdx].rx / (1024 * 1024),1)) + "MB\n"
+message += "transmitted: " + str(round(traffic.day[dayLastIdx].tx / (1024 * 1024), 1)) + "MB\n"
 message += "=========================\n"
-message += "(monthly) " + str(traffic.month[0].date.year) + "-" + str(traffic.month[0].date.month) + "\n" 
-message += "received: " + str(round(traffic.month[0].rx / (1024 * 1024),1)) + "MB\n"
-message += "transmitted: " + str(round(traffic.month[0].tx / (1024 * 1024), 1)) + "MB\n"
+message += "(monthly) " + str(traffic.month[monLastIdx].date.year) + "-" + str(traffic.month[monLastIdx].date.month) + "\n" 
+message += "received: " + str(round(traffic.month[monLastIdx].rx / (1024 * 1024),1)) + "MB\n"
+message += "transmitted: " + str(round(traffic.month[monLastIdx].tx / (1024 * 1024), 1)) + "MB\n"
 
 print(message)
 
